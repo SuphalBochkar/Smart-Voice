c3b683fc06885eff33a2c564a959ce27b2d3f373
Suphal Bochkar <suphalbochkar@gmail.com>
2025-02-22 16:46:03 +0530

    Project

---
+++ smart/app/(tabs)/_layout.tsx
@@ -0,0 +1,50 @@
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+
+import { HapticTab } from "@/smart/components/HapticTab";
+import { IconSymbol } from "@/smart/components/ui/IconSymbol";
+import TabBarBackground from "@/smart/components/ui/TabBarBackground";
+import { Colors } from "@/smart/constants/Colors";
+import { useColorScheme } from "@/smart/hooks/useColorScheme";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: TabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: "absolute",
+          },
+          default: {},
+        }),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="paperplane.fill" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
